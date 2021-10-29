bbe31e707f2085313e8d122d27df263761afa67c
Matheus Romero Quintans Limeira <kalilquintans565@gmail.com>
2021-10-29 19:17:47 -0300

    interfaceDoCadastro

--- telasIniciais.py
+++ telasIniciais.py
@@ -73,41 +73,41 @@ class janelaInicial():
 
             def continuar():     
                 def erro():
-                    topErro= Toplevel(self.telaCadastro)
+                    topErro= Toplevel(self.telaCadastro, bg= '#111111')
                     topErro.geometry('350x150+480+250')
                         
-                    Label(topErro, text= "ERRO! ALGUM ESPAÇAMETRO\n INCOMPLETO!").place(rely=0.2, relx=0.25)
-                    Button(topErro, text= 'OK', command=lambda: topErro.destroy()).place(rely=0.5, relx=0.43, relwidth=0.15)
+                    Label(topErro, text= " ERRO! ALGUM ESPAÇAMETRO\n INCOMPLETO!", bg= '#111111', fg= 'red').place(rely=0.2, relx=0.25)
+                    Button(topErro, text= 'OK', bg= '#212121', fg= 'white', border=0, command=lambda: topErro.destroy()).place(rely=0.5, relx=0.43, relwidth=0.15)
 
                 def userConfirmar():
                     if user.get() == '' or senha.get() == '':
                         erro()
                     else:
                         topUser.destroy()
-                        topConfirmar= Toplevel(self.telaCadastro)
+                        topConfirmar= Toplevel(self.telaCadastro, bg= '#111111')
                         topConfirmar.geometry('250x150+530+250')
 
-                        Label(topConfirmar, text= 'Desejar Realmente Fazer O Cadastro?').place(rely=0.1,relx=0.1)
-                        Button(topConfirmar, text= 'Sim', command= salvar).place(rely=0.5, relx=0.3)
-                        Button(topConfirmar, text= 'Não', command= voltarInicial).place(rely=0.5, relx=0.6)
+                        Label(topConfirmar, text= 'Desejar Realmente Fazer O Cadastro?', bg= '#111111', fg= 'white').place(rely=0.1,relx=0.1)
+                        Button(topConfirmar, text= 'Sim', bg= "#212121", fg= 'white', border=0, command= salvar).place(rely=0.5, relx=0.3, relwidth=0.15)
+                        Button(topConfirmar, text= 'Não', bg= '#212121', fg= 'white', border=0, command= voltarInicial).place(rely=0.5, relx=0.6, relwidth=0.15)
 
                 cont = 0
-                listaDados= [nome, idade, endereco, cpfCnpj, rg, tempoContrato, inicioContrato, mediaServico]
+                listaDados= [respostaRecuperacao, nome, idade, endereco, cpfCnpj, rg, tempoContrato, inicioContrato, mediaServico]
                 for dados in listaDados:
                     if dados.get() == '':
                         cont += 1
                 if cont > 0:
                     erro()
                 if cont == 0:
-                    topUser= Toplevel(self.telaCadastro)
+                    topUser= Toplevel(self.telaCadastro, bg= '#111111')
                     topUser.geometry('350x150+480+250')
 
-                    Label(topUser, text= 'Usuário:').place(rely=0.3, relx=0.1)
-                    Label(topUser, text= 'Senha').place(rely=0.4, relx=0.1)
+                    Label(topUser, text= 'Usuário:', bg= '#111111', fg= 'white').place(rely=0.3, relx=0.1)
+                    Label(topUser, text= 'Senha', bg= '#111111', fg= 'white').place(rely=0.4, relx=0.1)
                     user= Entry(topUser)
                     senha= Entry(topUser)
-                    Button(topUser, text= 'Confirmar',command= userConfirmar).place(rely=0.7, relx=0.1)
-                    Button(topUser, text= 'Cancelar', command=lambda: topUser.destroy()).place(rely=0.7, relx=0.7)
+                    Button(topUser, text= 'Confirmar', bg= "#212121", fg= 'white', border=0, command= userConfirmar).place(rely=0.7, relx=0.1)
+                    Button(topUser, text= 'Cancelar',bg= '#212121', fg= 'white', border=0, command=lambda: topUser.destroy()).place(rely=0.7, relx=0.7)
 
                     user.place(rely=0.3, relx=0.25, relwidth=0.4)
                     senha.place(rely=0.4, relx=0.25, relwidth=0.4)
@@ -117,40 +117,50 @@ class janelaInicial():
             self.telaCadastro= Tk()
             self.telaCadastro.geometry('300x600+420+50')
             self.telaCadastro.minsize(450, 600)
+            self.telaCadastro['bg']= '#313131'
             
-            nome= Entry(self.telaCadastro)
-            idade= Entry(self.telaCadastro)
-            endereco= Entry(self.telaCadastro)
-            cpfCnpj= Entry(self.telaCadastro)
-            rg= Entry(self.telaCadastro)
-            servico= Entry(self.telaCadastro)
-            tempoContrato= Entry(self.telaCadastro)
-            inicioContrato= Entry(self.telaCadastro)
-            mediaServico= Entry(self.telaCadastro)
+            nome= Entry(self.telaCadastro, bg= '#111111', fg= 'white')
+            idade= Entry(self.telaCadastro, bg= '#111111', fg= 'white')
+            endereco= Entry(self.telaCadastro, bg= '#111111', fg= 'white')
+            cpfCnpj= Entry(self.telaCadastro, bg= '#111111', fg= 'white')
+            rg= Entry(self.telaCadastro, bg= '#111111', fg= 'white')
+            servico= Entry(self.telaCadastro, bg= '#111111', fg= 'white')
+            tempoContrato= Entry(self.telaCadastro, bg= '#111111', fg= 'white')
+            inicioContrato= Entry(self.telaCadastro, bg= '#111111', fg= 'white')
+            mediaServico= Entry(self.telaCadastro, bg= '#111111', fg= 'white')
+            respostaRecuperacao= Entry(self.telaCadastro, bg= '#111111', fg= 'white')
             
-            nome.place(rely=0.095, relx=0.15, relwidth=0.8)
-            idade.place(rely=0.125, relx=0.15, relwidth=0.8)
-            endereco.place(rely=0.155, relx=0.2, relwidth=0.75)
+            nome.place(rely=0.1, relx=0.15, relwidth=0.8)
+            idade.place(rely=0.13, relx=0.15, relwidth=0.8)
+            endereco.place(rely=0.16, relx=0.2, relwidth=0.75)
             cpfCnpj.place(rely=0.19, relx=0.2, relwidth=0.75)
-            rg.place(rely=0.225, relx=0.1, relwidth=0.85)
-            servico.place(rely=0.255, relx=0.25, relwidth=0.7)
-            tempoContrato.place(rely=0.285, relx=0.3, relwidth=0.65)
-            inicioContrato.place(rely=0.315, relx=0.32, relwidth=0.63)
-            mediaServico.place(rely=0.345, relx=0.35, relwidth=0.6)
+            rg.place(rely=0.22, relx=0.2, relwidth=0.75)
+            servico.place(rely=0.25, relx=0.25, relwidth=0.7)
+            tempoContrato.place(rely=0.28, relx=0.3, relwidth=0.65)
+            inicioContrato.place(rely=0.31, relx=0.32, relwidth=0.63)
+            mediaServico.place(rely=0.34, relx=0.35, relwidth=0.6)
+            respostaRecuperacao.place(rely=0.55, relx=0.37, relwidth=0.6)
 
-            Label(self.telaCadastro, text= 'Nome').place(rely=0.095, relx=0.05)
-            Label(self.telaCadastro, text= 'Idade').place(rely=.125, relx=0.05)
-            Label(self.telaCadastro, text= 'Endereço').place(rely=0.155, relx=0.05)
-            Label(self.telaCadastro, text= 'CPF/CNPJ').place(rely=0.185, relx=0.05)
-            Label(self.telaCadastro, text= 'RG').place(rely=0.225, relx=0.05)
-            Label(self.telaCadastro, text= 'Tipo de Serviço').place(rely=0.255, relx=0.05)                    
-            Label(self.telaCadastro, text= 'Período de Serviço').place(rely=0.285, relx=0.05)
-            Label(self.telaCadastro, text= 'Data da Contratação').place(rely=0.315, relx=0.05)
-            Label(self.telaCadastro, text= 'Valor Médio Do Serviço').place(rely=0.345, relx=0.05)
-        
+            nome.focus()
 
-            self.cadastrarBotao= Button(self.telaCadastro, text= 'Continuar', command= continuar).place(rely=0.9, relx=0.08)
-            self.botaoVoltar= Button(self.telaCadastro, text= 'Voltar', command= voltarInicial).place(rely=0.9, relx=0.85)
+            listaPerguntas = ['Nome do cachorro', 'Qual cor você gosta?', 'Jogo prefirido', 'Comida preferido', 'Nome da escola', 'Nome do esporte favorito']
+
+            Label(self.telaCadastro, text= 'Nome', bg= '#313131').place(rely=0.1, relx=0.05)
+            Label(self.telaCadastro, text= 'Idade', bg= '#313131').place(rely=.13, relx=0.05)
+            Label(self.telaCadastro, text= 'Endereço', bg= '#313131').place(rely=0.16, relx=0.05)
+            Label(self.telaCadastro, text= 'CPF/CNPJ', bg= '#313131').place(rely=0.19, relx=0.05)
+            Label(self.telaCadastro, text= 'RG', bg= '#313131').place(rely=0.22, relx=0.05)
+            Label(self.telaCadastro, text= 'Tipo de Serviço', bg= '#313131').place(rely=0.25, relx=0.05)                    
+            Label(self.telaCadastro, text= 'Período de Serviço', bg= '#313131').place(rely=0.28, relx=0.05)
+            Label(self.telaCadastro, text= 'Data da Contratação', bg= '#313131').place(rely=0.31, relx=0.05)
+            Label(self.telaCadastro, text= 'Valor Médio Do Serviço', bg= '#313131').place(rely=0.34, relx=0.05)
+            Label(self.telaCadastro, text= 'Pergunta de Recupeção de Conta', bg= '#313131').place(rely=0.5, relx=0.05)
+            spinBoxRecuperacao= Spinbox(self.telaCadastro, values=listaPerguntas, wrap= True, bg= '#111111', fg= 'white')
+            spinBoxRecuperacao.place(rely=0.5, relx=0.47, relwidth= 0.5)
+            Label(self.telaCadastro, text= 'Resposta Para a Pergunta', bg= '#313131').place(rely=0.55, relx=0.05)
+
+            self.cadastrarBotao= Button(self.telaCadastro, text= 'Continuar', bg= '#111111', fg= 'white', border=0, command= continuar).place(rely=0.9, relx=0.08, relwidth=0.2)
+            self.botaoVoltar= Button(self.telaCadastro, text= 'Voltar', bg= '#111111', fg= 'white', border=0, command= voltarInicial).place(rely=0.9, relx=0.8, relwidth=0.2)
 
             self.telaCadastro.mainloop()
 
@@ -176,6 +186,8 @@ class janelaInicial():
         self.botaoLogar= Button(self.telaInicial, text= 'Logar',border=0, fg= 'white', bg= '#414141', command= logar).place(rely=0.55, relx=0.63)
         self.botaoCadastro= Button(self.telaInicial, text= 'Cadastrar', border=0, bg= 'black', fg= 'white',  command= cadastro).place(rely=0.85, relx=0.05)
         self.botaoSeEsq= Button(self.telaInicial, text= 'Esqueceu a Senha?', border=0, bg= 'black', fg= 'white', command= esqueciSenha).place(rely=0.85, relx=0.75)
+
+        self.entryUser.focus()
 
         self.telaInicial.mainloop()
 
